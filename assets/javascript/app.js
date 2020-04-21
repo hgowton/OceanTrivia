25237e4c96392969c03025d0aa788b59ff4ff9e4
hgowton <hgowton@gmail.com>
2020-04-21 15:53:10 -0400

    different pictures based on correct and incorrect answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 var card = $("#quiz-area");
 var score = $("#results-area")
-var countStart = 30;
+var countStart = 5;
 var correct = 0;
 var incorrect = 0;
 var currentQuestion = 0;
@@ -10,25 +10,25 @@ var questions = [{
     question: "Our oceans cover more than ______ of the Earth’s surface.",
     answers: ["60%", "65%", "70%", "75%"],
     correctAnswer: "70%",
-    image: "assets/images/earth.jpg",
+    image: ["assets/images/earth.jpg","assets/images/jellyfish.gif"],
     extraInfo: "With so much of the Earth’s surface taken up by ocean, it’s evident how vital these marine environments are to the planet, and how much there still is to be explored."
 }, {
     question: "The majority of life on Earth is __________",
     answers: ["aquatic", "benthic", "terrestrial", "nocturnal"],
     correctAnswer: "aquatic",
-    image: "assets/images/biodiversity.png",
+    image: ["assets/images/biodiversity.png", "assets/images/octopus.gif"],
     extraInfo: "As so much of the Earth’s surface is underwater, it comes as no surprise that marine species outnumber those on land. But, it’s an incredible 94 per cent of the Earth’s living species that exist within the oceans."
 },{
     question: "Where is the world's largest mountain chain?",
     answers: ["India", "Antartica", "under water", "Africa"],
     correctAnswer: "under water",
-    image: "assets/images/mountain.jpg",
+    image: ["assets/images/mountain.jpg", "assets/images/turtle.gif"],
     extraInfo : "Earth’s longest chain of mountains, the Mid-Ocean Ridge, is almost entirely beneath the ocean, stretching across a distance of 65,000 kilometres. It’s said that this mountain chain is less explored than the surface of Venus or Mars"
 },{
     question: "The copper found in this organism's blood has helped it become a key research specimen.",
     answers: ["horseshoe crab", "blue crab", "jellyfish", "octupus"],
     correctAnswer: "horseshoe crab",
-    image: "assets/images/copper.jpg",
+    image: ["assets/images/copper.jpg", "assets/images/surfing.gif"],
     extraInfo : "Horseshoe crabs use hemocyanin to carry oxygen through their blood. Because of the copper present in hemocyanin, their blood is blue. Their blood contains amebocytes, which play a similar role to the white blood cells of vertebrates in defending the organism against pathogens."
 }]
 
@@ -38,7 +38,6 @@ var timer;
 //variable to setup game functionality
 var game = {
     questions: questions,
-    // currentQuestion: 0,
     counter: countStart,
 
     countdown: function () {
@@ -137,7 +136,7 @@ var game = {
             $("<p>").html("<span class='info'> Score: </span>" + correct)
         )
         var image = $("<div class='col-md-6 oceanImage'>").append(
-            $("<img class='img-fluid'/>").attr("src", questions[currentQuestion].image).attr("alt", "ocean image")
+            $("<img class='img-fluid'/>").attr("src", questions[currentQuestion].image[0]).attr("alt", "ocean image")
             );
         var row = $("<div class='row'>").append(answer, image);
         score.append(row);
@@ -158,7 +157,7 @@ var game = {
             $("<p>").html("<span class='info'> Did you know: </span>" + questions[currentQuestion].extraInfo)
         )
         var image = $("<div class='col-md-6 oceanImage'>").append(
-            $("<img class='img-fluid'/>").attr("src", questions[currentQuestion].image).attr("alt", "ocean image")
+            $("<img class='img-fluid'/>").attr("src", questions[currentQuestion].image[1]).attr("alt", "ocean image")
             );
 
         var row = $("<div class='row'>").append(answer, image);
